ad7fee1eb10de34e75397de65c4bf4437f98440f
gruan01 <gruan@asnum.com>
2015-06-05 10:29:14 +0800

    Example

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XXY.WxApi.Entities;
+using XXY.WxApi;
+using XXY.WxApi.Methods;
+using XXY.WxApi.Entities.Menus;
+using System.Xml;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using XXY.WxApi.Entities.Requests;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace UnitTestProject1 {
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestInitialize]
+        public void Init() {
+            var cfgs = new List<ApiConfig>() {
+                new ApiConfig("tag","your appID", "your secret code", "your aes key , if not set , can be null", "your token")
+            };
+            ApiClient.Init(cfgs);
+        }
+
+        [TestMethod]
+        public void GetMenuTest() {
+            var client = ApiClient.GetInstance("tag");
+            var method = new MenuGet();
+            var data = client.Execute(method);
+        }
+
+        [TestMethod]
+        public void DeleteMenuTest() {
+            var client = ApiClient.GetInstance("tag");
+            var method = new MenuDelete();
+            var data = client.Execute(method);
+        }
+
+        [TestMethod]
+        public void CreateMenuTest() {
+            var client = ApiClient.GetInstance("tag");
+            var method = new MenuCreate() {
+                Menu = new Menu() {
+                    Buttons = new List<BaseMenu>() {
+                        new ClickButton(){ Name = "查询说明",Key = "Help"},
+                        //new ViewButton(){Name="百度", Url="http://www.baidu.com"},
+                        //new SubMenus(){ Name = "abc", SubButtons = new List<ButtonMenu>(){
+                        //    new ClickButton(){Name="A", Key = "A"},
+                        //    new ClickButton(){Name="B", Key="B"},
+                        //    new ClickButton(){Name="C", Key="C"}
+                        //}}
+                    }
+                }
+            };
+
+            var data = client.Execute(method);
+        }
+    }
+}
---
+++ UnitTestProject1/UserTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XXY.WxApi;
+using XXY.WxApi.Entities;
+using XXY.WxApi.Entities.Messages;
+using XXY.WxApi.Methods;
+
+namespace UnitTestProject1 {
+    [TestClass]
+    public class UserTest {
+
+        [TestInitialize]
+        public void Init() {
+            var cfgs = new List<ApiConfig>() {
+                new ApiConfig("tag1","your appID 1", "your secret code 1", "your aes key 1 , if not set , can be null", "your token 1"),
+                new ApiConfig("tag2","your appID 2", "your secret code 2", "your aes key 2 , if not set , can be null", "your token 2")
+            };
+            ApiClient.Init(cfgs);
+        }
+
+
+        [TestMethod]
+        public void GetUserInfo() {
+            var method = new UserInfo() {
+                OpenID = "replace with your User OpenID"
+            };
+            var client = ApiClient.GetInstance("test");
+            var user = client.Execute(method);
+        }
+
+        [TestMethod]
+        public void MessageSend() {
+            var method = new MessageSend() {
+                OpenID = "replace with your User OpenID",
+                Message = new TextMessage() {
+                    Content = "tttt"
+                }
+            };
+
+            var method2 = new MessageSend() {
+                OpenID = "replace with your User OpenID",
+                Message = new NewsMessage() {
+                    Articles = new List<Article>() {
+                         new Article(){
+                            Title = "文章1",
+                            Url = "http://mp.weixin.qq.com/wiki/1/70a29afed17f56d537c833f89be979c9.html",
+                            Description = "Description",
+                            PicUrl = "http://e.hiphotos.baidu.com/image/pic/item/b999a9014c086e06312ffb4200087bf40ad1cb30.jpg"
+                        },
+                        new Article(){
+                            Title = "文章2",
+                            Url = "http://mp.weixin.qq.com/wiki/1/70a29afed17f56d537c833f89be979c9.html",
+                            Description = "Description",
+                            PicUrl = "http://e.hiphotos.baidu.com/image/pic/item/b999a9014c086e06312ffb4200087bf40ad1cb30.jpg"
+                        }
+                    }
+                }
+            };
+
+            var client = ApiClient.GetInstance("tag1");
+            //var obj = client.Execute(method);
+            var obj2 = client.Execute(method2);
+        }
+
+    }
+}
